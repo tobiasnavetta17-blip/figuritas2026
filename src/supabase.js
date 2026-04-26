f161e380a28d53492eca190a6aa0616ca75e535f
tobiasnavetta17@gmail.com <tobiasnavetta17@gmail.com>
2026-04-26 14:18:22 -0300

    update supabase anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://pglklqrykhrhsbcitibi.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnbGtscXJ5a2hyaHNiY2l0aWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjY5NzAsImV4cCI6MjA2MTI0Mjk3MH0.YmFzZSIsInJlZiI6InBnbGtscXJ5a2hyaHNiY2l0aWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NjY5NzAsImV4cCI6MjA2MTI0Mjk3MH0";
+const supabaseUrl = 'https://pglklqrykhrhsbcitibi.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnbGtscXJ5a2hyaHNiY2l0aWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMTIzMTMsImV4cCI6MjA5Mjc4ODMxM30.lk8-mtLkS6ukPAM-iW-NRUJl0cWGME8eW9XEl-aHgs4'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
